15b677abc68dcac4e46d6f875ab0de84bcbe611d
TAKAHASHI Satoshi <hikobae@gmail.com>
2024-09-29 19:37:49 +0900

    ユーザーを CSV (users.csv) で指定可能にする

--- README.md
+++ README.md
@@ -2,8 +2,39 @@
 
 csv ファイルからシェルスクリプトを生成する.
 
+## Usage
+
 ```sh
-$ csv2sh a.csv
+$ cat command.csv
+COMMAND,ARG1,ARG2,ARG3,ARG4
+commit,2014/02/03 03:40:00,taro,../input/1,test
+commit,2014/12/03 15:32:00,taro,../input/2,test2
+tag,,,,docs/ios/1.0.0
+commit,2015/01/12 23:59:59,hanako,../input/3,Update files
+
+$ cat users.csv
+ID,NAME,EMAIL
+taro,OSAKA Taro,taro@example.com
+hanako,Tokyo Hanako,hanako@example.org
+
+$ csv2sh command.csv users.csv
+mkdir out
+pushd out
+git init
+find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
+cp -r ../input/1/. .
+git add -A
+GIT_COMMITTER_NAME="OSAKA Taro" GIT_COMMITTER_EMAIL="taro@example.com" GIT_COMMITTER_DATE="2014-02-03T03:40:00" GIT_AUTHOR_NAME="OSAKA Taro" GIT_AUTHOR_EMAIL="taro@example.com" GIT_AUTHOR_DATE="2014-02-03T03:40:00" git commit -m "test"
+find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
+cp -r ../input/2/. .
+git add -A
+GIT_COMMITTER_NAME="OSAKA Taro" GIT_COMMITTER_EMAIL="taro@example.com" GIT_COMMITTER_DATE="2014-12-03T15:32:00" GIT_AUTHOR_NAME="OSAKA Taro" GIT_AUTHOR_EMAIL="taro@example.com" GIT_AUTHOR_DATE="2014-12-03T15:32:00" git commit -m "test2"
+git tag docs/ios/1.0.0
+find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
+cp -r ../input/3/. .
+git add -A
+GIT_COMMITTER_NAME="Tokyo Hanako" GIT_COMMITTER_EMAIL="hanako@example.org" GIT_COMMITTER_DATE="2015-01-12T23:59:59" GIT_AUTHOR_NAME="Tokyo Hanako" GIT_AUTHOR_EMAIL="hanako@example.org" GIT_AUTHOR_DATE="2015-01-12T23:59:59" git commit -m "Update files"
+popd
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,55 @@ struct User {
 }
 
 impl User {
-    fn new(name: &str, mail: &str) -> Self {
+    fn new(name: String, mail: String) -> Self {
         User {
-            name: name.to_string(),
-            mail: mail.to_string(),
+            name: name,
+            mail: mail,
         }
     }
 }
 
 fn main() {
-    if env::args().len() != 2 {
-        eprintln!("Usage: csv2sh CSV_FILE");
+    if env::args().len() != 3 {
+        eprintln!("Usage: csv2sh COMMAND_CSV USERS_CSV");
         std::process::exit(1);
     }
 
-    let csv_path = env::args().nth(1).unwrap();
+    let command_csv_path = env::args().nth(1).unwrap();
+    let users_csv_path = env::args().nth(2).unwrap();
 
-    if let Err(err) = csv_file2sh(&csv_path) {
+    if let Err(err) = csv_file2sh(&command_csv_path, &users_csv_path) {
         eprintln!("{:#?}", err);
         std::process::exit(1);
     }
 }
 
-fn new_users() -> HashMap<String, User> {
+fn new_users(users_csv: &str) -> Result<HashMap<String, User>> {
     let mut users = HashMap::new();
-    users.insert(
-        String::from("taro"),
-        User::new("Test Test", "test.test@example.com"),
-    );
-    users
+
+    type Record = (String, String, String);
+    let mut rdr = csv::Reader::from_reader(users_csv.as_bytes());
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        users.insert(record.0, User::new(record.1, record.2));
+    }
+    Ok(users)
 }
 
-fn csv_file2sh(csv_path: &str) -> Result<()> {
-    let csv = fs::read_to_string(csv_path)?;
-    println!("{}", csv2sh(&csv)?);
+fn csv_file2sh(command_csv_path: &str, users_csv_path: &str) -> Result<()> {
+    let command_csv = fs::read_to_string(command_csv_path)?;
+    let users_csv = fs::read_to_string(users_csv_path)?;
+    println!("{}", csv2sh(&command_csv, &users_csv)?);
     Ok({})
 }
 
-fn csv2sh(csv: &str) -> Result<String> {
+fn csv2sh(command_csv: &str, users_csv: &str) -> Result<String> {
     let output_dir = "out";
     if Path::new(output_dir).exists() {
         return Err(anyhow!("Directory {output_dir} already exists"));
     }
 
-    let users = new_users();
+    let users = new_users(&users_csv)?;
 
     let mut sh: String = String::new();
     sh += &format!("mkdir {output_dir}\n");
@@ -69,7 +74,7 @@ fn csv2sh(csv: &str) -> Result<String> {
         Option<String>,
         Option<String>,
     );
-    let mut rdr = csv::Reader::from_reader(csv.as_bytes());
+    let mut rdr = csv::Reader::from_reader(command_csv.as_bytes());
     for result in rdr.deserialize() {
         let record: Record = result?;
         match record.0.to_lowercase().as_str() {
@@ -89,7 +94,7 @@ fn csv2sh(csv: &str) -> Result<String> {
                 sh += "find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf\n";
                 sh += &format!("cp -r {input} .\n");
                 sh += "git add -A\n";
-                sh += &format!("GIT_COMMITTER_NAME=\"{name}\" GIT_COMMITTER_EMAIL=\"{mail}\" GIT_COMMITTER_DATE=\"{date}\" GIT_AUTHOR_NAME=\"{name}\" GIT_AUTHOR_EMAIL=\"{mail}\" GIT_AUTHOR_DATE=\"{date}\" git commit -m {comment}\n", name=user.name, mail=user.mail, date=date);
+                sh += &format!("GIT_COMMITTER_NAME=\"{name}\" GIT_COMMITTER_EMAIL=\"{mail}\" GIT_COMMITTER_DATE=\"{date}\" GIT_AUTHOR_NAME=\"{name}\" GIT_AUTHOR_EMAIL=\"{mail}\" GIT_AUTHOR_DATE=\"{date}\" git commit -m \"{comment}\"\n", name=user.name, mail=user.mail, date=date);
             }
             "tag" => sh += &format!("git tag {}\n", record.4.unwrap()),
             _ => return Err(anyhow!("Unknown command `{}`", record.0)),
@@ -105,10 +110,15 @@ mod tests {
 
     #[test]
     fn csv2sh_test() {
-        let input = r#"COMMAND,ARG1,ARG2,ARG3,ARG4
+        let command_csv = r#"COMMAND,ARG1,ARG2,ARG3,ARG4
 commit,2014/02/03 03:40:00,taro,../input/1,test
 commit,2014/12/03 15:32:00,taro,../input/2,test2
 tag,,,,docs/ios/1.0.0
+commit,2015/01/12 23:59:59,hanako,../input/3,Update files
+"#;
+        let users_csv = r#"ID,NAME,EMAIL
+taro,OSAKA Taro,taro@example.com
+hanako,Tokyo Hanako,hanako@example.org
 "#;
         let expect = r#"mkdir out
 pushd out
@@ -116,15 +126,19 @@ git init
 find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
 cp -r ../input/1/. .
 git add -A
-GIT_COMMITTER_NAME="Test Test" GIT_COMMITTER_EMAIL="test.test@example.com" GIT_COMMITTER_DATE="2014-02-03T03:40:00" GIT_AUTHOR_NAME="Test Test" GIT_AUTHOR_EMAIL="test.test@example.com" GIT_AUTHOR_DATE="2014-02-03T03:40:00" git commit -m test
+GIT_COMMITTER_NAME="OSAKA Taro" GIT_COMMITTER_EMAIL="taro@example.com" GIT_COMMITTER_DATE="2014-02-03T03:40:00" GIT_AUTHOR_NAME="OSAKA Taro" GIT_AUTHOR_EMAIL="taro@example.com" GIT_AUTHOR_DATE="2014-02-03T03:40:00" git commit -m "test"
 find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
 cp -r ../input/2/. .
 git add -A
-GIT_COMMITTER_NAME="Test Test" GIT_COMMITTER_EMAIL="test.test@example.com" GIT_COMMITTER_DATE="2014-12-03T15:32:00" GIT_AUTHOR_NAME="Test Test" GIT_AUTHOR_EMAIL="test.test@example.com" GIT_AUTHOR_DATE="2014-12-03T15:32:00" git commit -m test2
+GIT_COMMITTER_NAME="OSAKA Taro" GIT_COMMITTER_EMAIL="taro@example.com" GIT_COMMITTER_DATE="2014-12-03T15:32:00" GIT_AUTHOR_NAME="OSAKA Taro" GIT_AUTHOR_EMAIL="taro@example.com" GIT_AUTHOR_DATE="2014-12-03T15:32:00" git commit -m "test2"
 git tag docs/ios/1.0.0
+find . -not -path './.git*' -not -path '.' -print0 | xargs -0 rm -rf
+cp -r ../input/3/. .
+git add -A
+GIT_COMMITTER_NAME="Tokyo Hanako" GIT_COMMITTER_EMAIL="hanako@example.org" GIT_COMMITTER_DATE="2015-01-12T23:59:59" GIT_AUTHOR_NAME="Tokyo Hanako" GIT_AUTHOR_EMAIL="hanako@example.org" GIT_AUTHOR_DATE="2015-01-12T23:59:59" git commit -m "Update files"
 popd
 "#;
-        let result = csv2sh(input).unwrap();
+        let result = csv2sh(&command_csv, &users_csv).unwrap();
         assert_eq!(result, expect);
     }
 }
